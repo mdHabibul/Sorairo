090594328fdc3edcb4a3d726fa80b852ac87fb0f
Alif <mdhba2023@gmail.com>
2026-09-10 16:02:08 +0600

    Boilerplate and install Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+import '.index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
